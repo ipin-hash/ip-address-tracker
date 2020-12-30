32438562bedefe8925ef7ed84ebac5f723f33125
ipin-hash <syamsullarifin03@gmail.com>
2020-12-30 13:26:20 +0700

    memperbaiki bug

--- main.js
+++ main.js
@@ -1,18 +1,33 @@
 const btn = document.querySelector(".btn");
+let map
 
+function getMaps() {
+  return fetch(`https://geo.ipify.org/api/v1?apiKey=at_2J51hbyWtQkOvCX6PRzCxdiCbl1lK`, {
+      method: 'GET'
+  }) 
+    .then(res => res.json())
+    .then(function(data) {
 
-function maps(data) {
-  let lat  = data.location.lat;
-  let lng = data.location.lng; 
-  const map = L.map("map").setView([lat ,lng], 13);
-  L.tileLayer("https://api.mapbox.com/styles/v1/ipinn/ckizms9bb7ud719qkfeibd5tw/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiaXBpbm4iLCJhIjoiY2tpem16azBwMmswNzJzbjRpYjV6amNmMiJ9.-kLfqnUk5UQk37MBNjXWlg", {
-    zoom: 1
-  }).addTo(map);
+      const {
+        lat, 
+        lng
+      } = data.location;
+    
+      map = L.map("map").setView([lat,lng], 13);
       
-  L.marker([lat, lng]).addTo(map);
+      L.marker([lat, lng]).addTo(map);
+
+      L.tileLayer("https://api.mapbox.com/styles/v1/ipinn/ckizms9bb7ud719qkfeibd5tw/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiaXBpbm4iLCJhIjoiY2tpem16azBwMmswNzJzbjRpYjV6amNmMiJ9.-kLfqnUk5UQk37MBNjXWlg&ipAddress=8.8.8.8", {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        tileSize: 256,
+      }).addTo(map);
+
+      result(data)
+    })
 }
 
 
+getMaps()
 function result(data) {
   document.querySelector('.address').innerHTML = data.ip; 
   document.querySelector(".location").innerHTML = `${data.location.city}, ${data.location.country}`;
@@ -22,30 +37,41 @@ function result(data) {
 
 
 
-btn.addEventListener("click", function(event) {
-  event.preventDefault()
+btn.addEventListener("click", function(e) {
+  e.preventDefault()
 
   const input = document.querySelector(".input");
-  return fetch(`https://geo.ipify.org/api/v1?apiKey=at_2J51hbyWtQkOvCX6PRzCxdiCbl1lK&ipAddress=8.8.8.8&domain=${input.value}`, {
-    method: 'GET'
-  })
-    .then(responese => responese.json())
-    .then(function(data) {
-      if(input.value === '') {
-        return null
-      } else {
-        maps(data);
-        result(data);
-        input.value =  '';
-      }
-    })
-    .catch(err => err);
+  const inputData = input.value;
+  getAddress(inputData);
+  input.value = '';
   }
 )
 
+function getAddress(ip) {
+  const apiUrl = "https://geo.ipify.org/api/v1?"
+  const apiKey = "apiKey=at_2J51hbyWtQkOvCX6PRzCxdiCbl1lK";
+  return fetch(`${apiUrl}${apiKey}&domain=${ip}`,{
+    method: "GET"
+  })
+    .then(res => {
+      if(res.status != 200) {
+        alert("domain yang anda masukkan salah")
+      } else {
+        return res.json();
+      }
+    })
+    .then (data => {
+      const{
+        lat,
+        lng
+      } = data.location
 
+      L.marker([lat, lng]).addTo(map);
+
+      map.panTo(new L.LatLng(lat, lng))
+      result(data)
+    })
+    .catch(error => console.log(error))
+}
 
     
-
-
-
